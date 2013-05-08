b20eb4521e88457bc4eac1b3ebb0930da1ba4208
Robert Gruendler <r.gruendler@gmail.com>
2013-05-08 02:16:30 +0200

    first codeassist tests

--- com.dubture.editor.sass.ui/src/com/dubture/editor/sass/ui/contentassist/SassProposalProvider.xtend
+++ com.dubture.editor.sass.ui/src/com/dubture/editor/sass/ui/contentassist/SassProposalProvider.xtend
@@ -4,9 +4,21 @@
 package com.dubture.editor.sass.ui.contentassist
 
 import com.dubture.editor.sass.ui.contentassist.AbstractSassProposalProvider
+import org.eclipse.emf.ecore.EObject
+import org.eclipse.xtext.RuleCall
+import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor
 
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
 class SassProposalProvider extends AbstractSassProposalProvider {
+	
+	
+	override complete_VARIABLE(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		super.complete_VARIABLE(model, ruleCall, context, acceptor)
+		
+		acceptor.accept(createCompletionProposal("$foo", context));
+	}
+	
 }
--- com.dubture.editor.sass.ui/xtend-gen/com/dubture/editor/sass/ui/contentassist/SassProposalProvider.java
+++ com.dubture.editor.sass.ui/xtend-gen/com/dubture/editor/sass/ui/contentassist/SassProposalProvider.java
@@ -4,10 +4,20 @@
 package com.dubture.editor.sass.ui.contentassist;
 
 import com.dubture.editor.sass.ui.contentassist.AbstractSassProposalProvider;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
 @SuppressWarnings("all")
 public class SassProposalProvider extends AbstractSassProposalProvider {
+  public void complete_VARIABLE(final EObject model, final RuleCall ruleCall, final ContentAssistContext context, final ICompletionProposalAcceptor acceptor) {
+    super.complete_VARIABLE(model, ruleCall, context, acceptor);
+    ICompletionProposal _createCompletionProposal = this.createCompletionProposal("$foo", context);
+    acceptor.accept(_createCompletionProposal);
+  }
 }
